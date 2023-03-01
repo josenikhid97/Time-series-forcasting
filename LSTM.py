a1051c11c82dedb1c8e888c5a6895b8749b52a6d
Jose DASARI <64956458+josenikhid97@users.noreply.github.com>
2023-03-01 07:35:30 +0100

    Add files via upload

---
+++ LSTM.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import time
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Model, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.lstm = nn.LSTMCell(self.input_size, self.hidden_size)
+        self.linear = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, future=0, y=None):
+        outputs = []
+
+        # reset the state of LSTM
+        # the state is kept till the end of the sequence
+        h_t = torch.zeros(input.size(0), self.hidden_size, dtype=torch.float32)
+        c_t = torch.zeros(input.size(0), self.hidden_size, dtype=torch.float32)
+
+        for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
+            h_t, c_t = self.lstm(input_t, (h_t, c_t))
+            output = self.linear(h_t)
+            outputs += [output]
+
+        for i in range(future):
+            if y is not None and random.random() > 0.5:
+                output = y[:, [i]]  # teacher forcing
+            h_t, c_t = self.lstm(output, (h_t, c_t))
+            output = self.linear(h_t)
+            outputs += [output]
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs
+        
+
+
+class Optimization:
+    """ A helper class to train, test and diagnose the LSTM"""
+
+    def __init__(self, model, loss_fn, optimizer, scheduler):
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.train_losses = []
+        self.val_losses = []
+        self.futures = []
+
+    @staticmethod
+    def generate_batch_data(x, y, batch_size):
+        for batch, i in enumerate(range(0, len(x) - batch_size, batch_size)):
+            x_batch = x[i : i + batch_size]
+            y_batch = y[i : i + batch_size]
+            yield x_batch, y_batch, batch
+
+    def train(
+        self,
+        x_train,
+        y_train,
+        x_val=None,
+        y_val=None,
+        batch_size=10,
+        n_epochs=15,
+        do_teacher_forcing=None,
+    ):
+        seq_len = x_train.shape[1]
+        for epoch in range(n_epochs):
+            start_time = time.time()
+            self.futures = []
+
+            train_loss = 0
+            for x_batch, y_batch, batch in self.generate_batch_data(x_train, y_train, batch_size):
+                y_pred = self._predict(x_batch, y_batch, seq_len, do_teacher_forcing)
+                self.optimizer.zero_grad()
+                loss = self.loss_fn(y_pred, y_batch)
+                loss.backward()
+                self.optimizer.step()
+                train_loss += loss.item()
+            self.scheduler.step()
+            train_loss /= batch
+            self.train_losses.append(train_loss)
+
+            self._validation(x_val, y_val, batch_size)
+
+            elapsed = time.time() - start_time
+            print(
+                "Epoch %d Train loss: %.2f. Validation loss: %.2f. Avg future: %.2f. Elapsed time: %.2fs."
+                % (epoch + 1, train_loss, self.val_losses[-1], np.average(self.futures), elapsed)
+            )
+
+    def _predict(self, x_batch, y_batch, seq_len, do_teacher_forcing):
+        if do_teacher_forcing:
+            future = random.randint(1, int(seq_len) / 2)
+            limit = x_batch.size(1) - future
+            y_pred = self.model(x_batch[:, :limit], future=future, y=y_batch[:, limit:])
+        else:
+            future = 0
+            y_pred = self.model(x_batch)
+        self.futures.append(future)
+        return y_pred
+
+    def _validation(self, x_val, y_val, batch_size):
+        if x_val is None or y_val is None:
+            return
+        with torch.no_grad():
+            val_loss = 0
+            for x_batch, y_batch, batch in self.generate_batch_data(x_val, y_val, batch_size):
+                y_pred = self.model(x_batch)
+                loss = self.loss_fn(y_pred, y_batch)
+                val_loss += loss.item()
+            val_loss /= batch
+            self.val_losses.append(val_loss)
+
+    def evaluate(self, x_test, y_test, batch_size, future=1):
+        with torch.no_grad():
+            test_loss = 0
+            actual, predicted = [], []
+            for x_batch, y_batch, batch in self.generate_batch_data(x_test, y_test, batch_size):
+                y_pred = self.model(x_batch, future=future)
+                y_pred = (
+                    y_pred[:, -len(y_batch) :] if y_pred.shape[1] > y_batch.shape[1] else y_pred
+                )
+                loss = self.loss_fn(y_pred, y_batch)
+                test_loss += loss.item()
+                actual += torch.squeeze(y_batch[:, -1]).data.cpu().numpy().tolist()
+                predicted += torch.squeeze(y_pred[:, -1]).data.cpu().numpy().tolist()
+            test_loss /= batch
+            return actual, predicted, test_loss
+            
+            
+    def evaluate2(self, x_test, y_test, batch_size=1, future=1):
+        with torch.no_grad():
+            test_loss = 0
+            actual, predicted = [], []
+            for x_batch, y_batch, batch in self.generate_batch_data(x_test, y_test, batch_size):
+                y_pred = self.model(x_batch, future=future)
+                y_pred = (
+                    y_pred[:, -len(y_batch) :] if y_pred.shape[1] > y_batch.shape[1] else y_pred
+                )
+                loss = self.loss_fn(y_pred, y_batch)
+                test_loss += loss.item()
+                actual += torch.squeeze(y_batch[:, -1]).data.cpu().numpy().item(0)
+                predicted += torch.squeeze(y_pred[:, -1]).data.cpu().numpy().item(0)
+            test_loss /= batch
+            return actual, predicted, test_loss
+
+    
+
+    def plot_losses(self):
+        plt.plot(self.train_losses, label="Training loss")
+        plt.plot(self.val_losses, label="Validation loss")
+        plt.legend()
+        plt.title("MSE Losses")
+        
+    def plot_RMSElosses(self):
+        plt.plot(np.sqrt(self.train_losses), label="Training loss")
+        plt.plot(np.sqrt(self.val_losses), label="Validation loss")
+        plt.legend()
+        plt.title("RMSE Losses")
---
+++ ReadME.md
@@ -0,0 +1,27 @@
+**Prediction - ML LSTM:**
+
+1. Week_7_LSTM_Real_Data.ipynb
+2. Week_7_LSTM_EBM.ipynb
+3. Week_7_LSTM_ABM.ipynb
+
+**Prediction - prophet:**
+
+1. Week7_prophet_RTD.ipynb
+2. Week7_prophet_EBM.ipynb
+3. Week7_prophet_ABM.ipynb
+
+**Prediction -classical times series [ARIMA]:**
+
+1. Week_7_Real_data_classic_time_series_.ipynb 
+2. Week_7_EBM_data_classic_time_series.ipynb
+3. Week_7_ABM_data_classic_time_series.ipynb
+
+**Fitting ABM and EBM models and find RMSE**
+1. Week_7_SEIR_Fit_EBM.ipynb
+2. Week_7_SEIR_Fit_ABM.ipynb
+
+
+For running the Notebook please clone the Week 7 folder and update the path in the first block code in each Notebook file
+
+
+
